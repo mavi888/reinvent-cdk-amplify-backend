d6d16aa449d3db65c870c6925441415d38207730
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2021-10-01 15:57:36 +0300

    modifying the lambda function

--- lib/functions/markShoppingDone.ts
+++ lib/functions/markShoppingDone.ts
@@ -1,7 +1,9 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import {Context } from 'aws-lambda';
 
 export async function handler(event: any, context: Context): Promise<any> {
     console.log('start lambda function');
 
     console.log(event);
+
+    return [];
   }
